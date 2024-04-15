a77e155e7c84d716901618dea9f77618108e5f18
changui <122252160+changuii@users.noreply.github.com>
2024-04-15 13:52:56 +0900

    Time: 0 ms (100%), Space: 44.9 MB (40.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,33 +2,17 @@ import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        int[] temp = nums.clone();
-        Arrays.sort(nums);
-
-        int i = 0;
-        int j = nums.length-1;
-        while(i < j){
-            int sum = nums[i] + nums[j];
-            if(sum == target ) break;
-            else if(sum < target) i++;
-            else {
-                j--;
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i, j};
+                }
+                else if(nums[nums.length-1-i] + nums[nums.length-1-j] == target){
+                    return new int[] {nums.length-1-j, nums.length-1-i};
+                }
             }
         }
-        System.out.println(i + " " + j);
-        boolean flag1 = false;
-        boolean flag2 = false;
-        for(int x=0; x<temp.length; x++){
-            if(nums[i] == temp[x] && !flag1){ 
-                i = x;
-                flag1 = true;
-            }else if(nums[j] == temp[x] && !flag2){ 
-                j = x;
-                flag2 = true;
-            }
-            if(flag1 && flag2) return new int[] {i, j};
-        }
-        
-        return new int[] {i, j};
+        return new int[] {0, 0};
+       
     }
 }
