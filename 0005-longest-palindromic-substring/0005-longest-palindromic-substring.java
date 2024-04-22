f280faf2d354ed84a8029608d057c206da36a61c
changui <122252160+changuii@users.noreply.github.com>
2024-04-22 17:23:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String z = s;
+        s = "";
+        for(char x : z.toCharArray()){
+            s = s + "?" + x;
+        }
+        System.out.println(s);
+
+        String max = "";
+        for(int i=1; i<s.length()-1; i++){
+            String current = s.charAt(i)+"";
+            int a = 1;
+            while(i-a >= 0 && i + a < s.length() && s.charAt(i-a) == s.charAt(i+a)){
+                current = s.charAt(i-a) + current + s.charAt(i+a);
+                a++;
+            }
+            if(max.length() < current.length())
+                max = current;
+        }
+
+        z = "";
+        for(char x : max.toCharArray()){
+            z += x == '?' ? "" : x;
+        }
+
+        return z;
+    }
+}
