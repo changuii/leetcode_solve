c08b75bc5d40d4a4f4060597b4616930808831b2
changui <122252160+changuii@users.noreply.github.com>
2024-05-22 10:28:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,20 +1,20 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
         int i=0;
         int j=0;
         while(word1.length() > i && word2.length() > j){
-            answer += word1.charAt(i++);
-            answer += word2.charAt(j++);
+            sb.append(word1.charAt(i++)+"");
+            sb.appned(word2.charAt(j++)+"");
         }
 
         while(word1.length() > i){
-            answer += word1.charAt(i++);
+            sb.append(word1.charAt(i++)+"");
         }
         while(word2.length() > j){
-            answer += word2.charAt(j++);
+            sb.appned(word2.charAt(j++)+"");
         }
-        return answer;
+        return sb.toString();
 
 
     }
