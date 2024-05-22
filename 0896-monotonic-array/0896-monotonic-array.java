ff01f591d36c049bb9c743e68cee1e487b2d8226
changui <122252160+changuii@users.noreply.github.com>
2024-05-22 12:56:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,13 +1,17 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
         boolean s = nums[nums.length-1] - nums[0] > 0 ? true : false;
-        for(int i=1; i<nums.length; i++){
-            if(s){
+        if(s){
+            for(int i=1; i<nums.length; i++){
                 if(nums[i] - nums[i-1] < 0) return false;
-            }else{
+            }
+        }
+        }else{
+            for(int i=1; i<nums.length; i++){
                 if(nums[i] - nums[i-1] > 0) return false;
             }
         }
+        
         return true;
     }
 }
